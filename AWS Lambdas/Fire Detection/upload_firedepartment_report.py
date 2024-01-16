7e6625be585754cc8ec674f5ad3eb6f52df8e7d3
csaz9482 <luka.mirceta@student.uibk.ac.at>
2024-01-16 15:45:58 +0100

    LM: Upload Fire Detection files.

---
+++ AWS Lambdas/Fire Detection/upload_firedepartment_report.py
@@ -0,0 +1,29 @@
+#IMPORTANT: This Lambda function has to have a layer containing a Redis installation.
+import redis
+import json
+import uuid
+from datetime import datetime as dt
+def storeInDB(description, path):
+    # Establish connection and update hostname if needed
+    redis_client = redis.StrictRedis(host='ec2-54-90-241-143.compute-1.amazonaws.com', port=6379, password='1SQRr7hyIb7peXvdcT4pSV3iu7lykHVd2qmMm+aOUBvC/Xt3vuLCzNg2QkSztjdY')
+
+    unique_event_id = str(uuid.uuid4())
+    current_time = str(dt.now())
+    event_data = {
+    "eventId": unique_event_id,
+    "eventType": "Fire",
+    "timestamp": current_time,
+    "description": description,
+    "videoPath": path,
+    }
+
+    # Build the key for the event
+    event_key = f"{event_data['eventType']}:{event_data['eventId']}"
+
+    # Store the event in Redis
+    redis_client.hset("firedepartment_reports", event_key, json.dumps(event_data))
+
+def lambda_handler(event, context):
+    pathToVideo = event.get('pathToVideo')
+    description = "Fire detected"
+    storeInDB(description, pathToVideo)
